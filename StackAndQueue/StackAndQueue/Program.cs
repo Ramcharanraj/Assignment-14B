327e5d410068c742dcaa7f5b3ad1ea94174331e9
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-05 23:20:12 +0530

    ADDED: Creation of Stack

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creation of Stack
+            LinkedListStack stack = new LinkedListStack();
+
+            //Adding Values to the Stack
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+        }
+    }
+}
